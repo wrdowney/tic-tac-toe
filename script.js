ed301f11e94788584992870e40f9ee2d8ad84390
Will Downey <willd20@vt.edu>
2022-06-28 19:55:12 -0400

    Win and tie logic calculations

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 let gameSquares = document.querySelectorAll('.game-square');
 let gameMessage = document.querySelector('.game-message');
-
+let gameOver = false;
 
 const gameBoard = (() => {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    board = ["", "", "", "", "", "", "", "", ""];
 
     const getBoard = () => board;
 
     const resetBoard = () => {
         board.fill("");
     }
-    return {getBoard, resetBoard}
+
+    const checkWin = () => {
+        let win = false;
+        let winCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        winCombos.forEach(combo => {
+            if (board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]] && board[combo[0]] !== "") {
+                win = true;
+            }
+        });
+        return win;
+    }
+
+    return {getBoard, resetBoard, checkWin}
 })();
 
 const displayController = (() => {
     const update = () => {
         gameBoard.getBoard().forEach((cell, index) => {
             document.getElementById(`${index + 1}`).innerHTML = cell;
+            gameOver = false;
         });
     }
     return {update}
@@ -33,19 +55,31 @@ const playerFactory = (name, symbol) => {
 let playerX = playerFactory('X');
 let playerO = playerFactory('O');
 
+
 let currentPlayer = playerX
 
 gameSquares.forEach(square => {
     square.addEventListener('click', () => {
-        if(square.innerHTML === '') {
-            square.innerHTML = currentPlayer.name;
-            if (currentPlayer === playerX) {
-                currentPlayer = playerO;
+        if(!gameOver) {
+            if(square.innerHTML === '') {
+                gameBoard.getBoard()[square.id - 1] = currentPlayer.name;
+                displayController.update();
+                if (currentPlayer === playerX) {
+                    currentPlayer = playerO;
+                }
+                else {
+                    currentPlayer = playerX;
+                }
+                gameMessage.innerHTML = `Player ${currentPlayer.name}'s turn`;
+                if(gameBoard.checkWin()) {
+                    gameMessage.innerHTML = `Player ${currentPlayer.name} wins!`;
+                    gameOver = true;
+                }
+                else if(gameBoard.getBoard().every(cell => cell !== '')) {
+                    gameMessage.innerHTML = 'It\'s a tie!';
+                    gameOver = true;
+                }
             }
-            else {
-                currentPlayer = playerX;
-            }
-            gameMessage.innerHTML = `Player ${currentPlayer.name}'s turn`;
         }
     }
     )
