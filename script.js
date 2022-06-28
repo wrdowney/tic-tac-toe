5a2a9bd94b5cc7d0f2d084b8e0585e40a567964a
Will Downey <willd20@vt.edu>
2022-06-28 19:17:10 -0400

    DisplayController updates on changes to game board

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const gameBoard = (() => {
     const resetBoard = () => {
         board.fill("");
     }
+    return {getBoard, resetBoard}
 })();
 
 const displayController = (() => {
+    const update = () => {
+        gameBoard.getBoard().forEach((cell, index) => {
+            document.getElementById(`${index + 1}`).innerHTML = cell;
+        });
+    }
+    return {update}
+})(gameBoard);
 
-})();
+displayController.update();
 
 const playerFactory = (name, symbol) => {
     return {
